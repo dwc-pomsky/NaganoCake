5653002b87c742869da5c8d08182cee4de8df532
RinaAraki <arakipomu1206@gmail.com>
2022-01-16 09:18:47 +0000

    [add]viewの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+
   devise_for :costomers
+
+  get '/home/top' => 'homes#top'
+  get '/home/about' => 'homes#about'
+
+
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
