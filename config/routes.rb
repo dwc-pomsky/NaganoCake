a564692c4f67d3ff461e1b4dbc2043bda6e1ea31
RinaAraki <arakipomu1206@gmail.com>
2022-01-17 09:29:27 +0000

    [add]route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   get '/home/top' => 'public/homes#top'
   get '/home/about' => 'public/homes#about'
 
+  get '/items' => 'items#index'
+  get '/items/:id' => 'items/show'
+
 
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
