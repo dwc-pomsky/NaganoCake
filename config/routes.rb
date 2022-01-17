0863c427a414463c7acaa24c1aa6c7bd1d515d91
RinaAraki <arakipomu1206@gmail.com>
2022-01-17 09:33:42 +0000

    [add]route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,14 @@ Rails.application.routes.draw do
   get '/home/top' => 'public/homes#top'
   get '/home/about' => 'public/homes#about'
 
+  get '/items' => 'items#index'
+  get '/items/:id' => 'items/show'
+
+  get '/admin/customers' => 'admin/customers#index'
+  get '/admin.customers/:id' => 'admin/customers#show'
+  get '/admin/customers/:id/edit' => 'admin/customers#edit'
+  patch '/admin/customers/:id' => 'admin/customers/update'
+
 
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
