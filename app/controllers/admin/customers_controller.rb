4fa289a7deb4bb2accb00e504577b54d5c21df79
RinaAraki <arakipomu1206@gmail.com>
2022-01-17 08:30:28 +0000

    [add]

---
+++ app/controllers/admin/customers_controller.rb
@@ -0,0 +1,29 @@
+class Admin::CustomersController < ApplicationController
+  
+  def index
+    @customers = Customer.all
+  end
+  
+  def show 
+    @customer = Customer.find(params[:id])
+  end
+  
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    if @customer.update(customer_params)
+      redirect_to customer_path
+    else
+      render
+    end
+  end
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:id, :fullname , :email , :is_deleted)
+  end
+
+end
