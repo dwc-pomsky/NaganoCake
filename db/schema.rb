14ef8e9bb50072f96a05526b408ada3c42a8d545
RouxRhett <nsch.4871@gmail.com>
2022-01-19 12:36:58 +0900

    [change]スキーマファイルのバージョンをdevelopブランチと合わせました

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_18_114727) do
+ActiveRecord::Schema.define(version: 2022_01_17_082059) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -54,15 +54,6 @@ ActiveRecord::Schema.define(version: 2022_01_18_114727) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
-  end
-
-  create_table "delivery_addresses", force: :cascade do |t|
-    t.integer "customer_id", null: false
-    t.string "post_code", null: false
-    t.string "address", null: false
-    t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "genres", force: :cascade do |t|
