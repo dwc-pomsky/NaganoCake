091d612d6f3a3ed921985f5194f3eab6ff4fc46c
RinaAraki <arakipomu1206@gmail.com>
2022-01-20 05:16:30 +0000

    [add]ルーティングの設定/コメントアウトで記述の予想

--- db/migrate/20220119064114_create_cart_items.rb
+++ db/migrate/20220119064114_create_cart_items.rb
@@ -4,6 +4,9 @@ class CreateCartItems < ActiveRecord::Migration[5.2]
       t.integer :item_id,       null: false
       t.integer :customer_id,   null: false
       t.integer :amount,        null: false
+      
+      # reference型?foreign_key: true にすると勝手にインデックス張られる
+      # integerの状態でする場合は、add_foreign_key :対象のテーブル名, :指定先のテーブル になる(インデックスも勝手に張られる)
 
       t.timestamps
     end
