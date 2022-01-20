f2e64435cf34619f50aac4261a0629221fddf395
RinaAraki <arakipomu1206@gmail.com>
2022-01-20 06:04:24 +0000

    [add]アソシエーションの記述

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,9 +1,6 @@
 class CartItem < ApplicationRecord
 
-  # has_one :cart_itemsを、customer.rbに記述?
-  # belongs_to :customerをcart_item.rbに記述?
-
-  # belongs_to :cart_itemsを、item.rbに記述?
-  # has_many :itemsを、cart_item.rbに記述?
+  belongs_to :customer
+  has_many :items, dependent: :destroy
 
 end
