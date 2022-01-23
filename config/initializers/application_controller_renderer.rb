0eaee208118e0144c8e5b0b254a29d943d18eb32
RinaAraki <arakipomu1206@gmail.com>
2022-01-23 10:08:31 +0000

    [add]footer-layout

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'cba230afe7ed314f6db32036f6f1c21303556c7b951aa70053128c101116e54143289d53f0f6f53eff50abcf49e104df436716daee6be983b96cb8a85efc4858'
