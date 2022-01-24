1ffc358ac761af801ef71a3c6931dc3fc059b133
RouxRhett <57049361+RouxRhett@users.noreply.github.com>
2022-01-24 18:21:12 +0900

    Revert "注文履歴詳細画面　エラー修正"

--- app/controllers/admin/homes_controller.rb
+++
@@ -1,8 +0,0 @@
-class Admin::HomesController < ApplicationController
-  before_action :authenticate_admin!
-  def top
-     @orders = Order.page(params[:page]).reverse_order.per(10)
-     
-     @sum = 0
-  end
-end
