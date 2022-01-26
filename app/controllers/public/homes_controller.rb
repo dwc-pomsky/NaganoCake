5c0da2d458f2c82a7930167ef10bbab3fc910f9c
RouxRhett <nsch.4871@gmail.com>
2022-01-26 14:40:38 +0900

    [fix]顧客側の商品一覧の修正

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,7 @@
 class Public::HomesController < ApplicationController
 
   def top
-    @items = Item.page(params[:page]).reverse_order.per(4)
+    @items = Item.where(sales_status: true).page(params[:page]).reverse_order.per(4)
   end
 
   def about
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,7 @@
 class Public::ItemsController < ApplicationController
 
   def index
-    @items = Item.page(params[:page]).per(8)
+    @items = Item.where(sales_status: true).page(params[:page]).per(8)
     @item = Item.all
   end
 
